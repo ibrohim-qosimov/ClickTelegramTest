0964891583717b85659a0df7687f3c330620691d
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-02-03 21:05:32 +0500

    Add project files.

---
+++ ClickTelegramTest.Code/Models/SendInvoiceRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Payments;
+
+namespace ClickTelegramTest.Code.Models
+{
+    public class SendInvoiceRequest
+    {
+        public object ChatId { get; set; } // Foydalanuvchi ID yoki @channelusername
+        public int? MessageThreadId { get; set; } // Forum mavzu identifikatori (optional)
+        public string Title { get; set; } // Mahsulot nomi (1-32 ta belgi)
+        public string Description { get; set; } // Mahsulot tavsifi (1-255 ta belgi)
+        public string Payload { get; set; } // Bot tomonidan aniqlangan invoice payload (1-128 bayt)
+        public string ProviderToken { get; set; } // @BotFather dan olingan token
+        public string Currency { get; set; } // Uch harfli valyuta kodi (UZS, USD, EUR)
+        public List<LabeledPrice> Prices { get; set; } // Narx va boshqa tarkibiy qismlar ro‘yxati
+    }
+}
---
+++ ClickTelegramTest.Code/Program.cs
@@ -0,0 +1,93 @@
+﻿using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace ClickTelegramTest.Code
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("7306566615:AAHIbipJji08j9OshR25KxdfvyRHEKmBtTs");
+
+            using CancellationTokenSource cts = new();
+
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+            };
+
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+
+            //Har qanday o'zgarish shu method 
+            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                var handler = update.Type switch
+                {
+                    UpdateType.Message => HandleMessageAsync(botClient, update, cancellationToken),
+                    //Yana update larni davom ettirib tutishingiz mumkin
+                };
+
+                try
+                {
+                    await handler;
+                }
+                catch (Exception ex)
+                {
+                    await Console.Out.WriteLineAsync($"Xato:{ex.Message}");
+                }
+            }
+
+            //Polling errorlarni shu ushledi
+            Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+                var ErrorMessage = exception switch
+                {
+                    ApiRequestException apiRequestException
+                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                    _ => exception.ToString()
+                };
+
+                Console.WriteLine(ErrorMessage);
+                return Task.CompletedTask;
+            }
+        }
+
+        private static async Task HandleMessageAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            //betda update.Messageni null emasligini tekshirib,
+            //null bo'lmasa message degan o'zgaruvchiga qiymatni olib beryapti
+            if (update.Message is not { } message)
+                return;
+            if (message.Text is not { } messageText)
+                return;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {update.Message.Chat.Id}.");
+
+            if (messageText == "/start")
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    text: $"Assalomu alaykum <b> {update.Message.From.FirstName} </b>",
+                    parseMode: ParseMode.Html,
+                    cancellationToken: cancellationToken);
+            }
+
+        }
+
+    }
+}
